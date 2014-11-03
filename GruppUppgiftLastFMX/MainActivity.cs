78aec62fcb4bc39ff8284d8993286da6bef77830
JamesRecond <c.k.johansson@gmail.com>
2014-11-03 12:55:04 +0100

    aa

---
+++ GruppUppgiftLastFMX/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+
+
+namespace GruppUppgiftLastFMX
+{
+	[Activity (Label = "GruppUppgiftLastFMX", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+		//hallööö
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			//hallööö
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			//hallööö
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			//hallööö
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks!", count++);
+			};
+
+	
+		}
+	}
+}
+
+
